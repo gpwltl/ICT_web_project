d9e2eb59683c51d8a2988d256f3ade65777630ab
gpwltl <gpwltl97@naver.com>
2020-09-23 20:30:41 +0900

    delete test

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,9 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
+//import IconButton from '@material-ui/core/IconButton';
+//import InfoIcon from '@material-ui/icons/Info';
 import tileData from '../tileData'
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -35,9 +36,11 @@ const Games = ({ isOpen, close }) => {
                         <div className={classes.root}>
                             <GridList cellHeight={180} className={classes.gridList}>
                                 <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}></GridListTile>
-
+                                <Link exact to="/">HOME</Link>
+                                <Link exact to="/about">ABOUT</Link>
+                                <Link exact to="/review">REVIEW</Link>
                                 {tileData.map((tile) => (
-                                    <GridListTile key={tile.img}>
+                                    <GridListTile key={tile.img} >
                                         <img src={tile.img} alt={tile.title} />
                                         <GridListTileBar
                                             title={tile.title}
--- testtttttttttttt/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Review } from './Review';
-export { default as Login } from './login';
