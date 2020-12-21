899cd9990da6c76a5e02ff046870ffa8369b3489
gpwltl <gpwltl97@naver.com>
2020-12-21 16:04:30 +0900

    Updates

---
+++ precache-manifest.3c68256a834107fae1f621cd61f4448c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4a36b82e3ef176d5052d4f5802fa56",
+    "url": "/ICT_web_project/index.html"
+  },
+  {
+    "revision": "b9cfc0c60c4f9c420452",
+    "url": "/ICT_web_project/static/css/main.d9ab7440.chunk.css"
+  },
+  {
+    "revision": "b8bbcf67c9e3b6546195",
+    "url": "/ICT_web_project/static/js/2.e0b80a44.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ICT_web_project/static/js/2.e0b80a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cfc0c60c4f9c420452",
+    "url": "/ICT_web_project/static/js/main.0e79d499.chunk.js"
+  },
+  {
+    "revision": "41de2ed24c878d4de71f",
+    "url": "/ICT_web_project/static/js/runtime-main.b11dc277.js"
+  },
+  {
+    "revision": "e0e955380d93745a53e4f14b0eb258d7",
+    "url": "/ICT_web_project/static/media/baseball.e0e95538.jpg"
+  },
+  {
+    "revision": "ee1b5a8dab60898dc0dfed872a454471",
+    "url": "/ICT_web_project/static/media/dog.ee1b5a8d.jpg"
+  },
+  {
+    "revision": "ebe54587683fca2b0042031415c2ccb7",
+    "url": "/ICT_web_project/static/media/gugudan.ebe54587.jpg"
+  },
+  {
+    "revision": "f884d90e0816073dd9c70bb82900be57",
+    "url": "/ICT_web_project/static/media/logo2.f884d90e.png"
+  },
+  {
+    "revision": "0bb27b07f4ab030d05fe169178a88f5b",
+    "url": "/ICT_web_project/static/media/lottery.0bb27b07.jpg"
+  },
+  {
+    "revision": "53ad121b3dda14db8387535243ca22a4",
+    "url": "/ICT_web_project/static/media/wordchain.53ad121b.jpg"
+  }
+]);
